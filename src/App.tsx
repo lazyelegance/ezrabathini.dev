4fde78424f7fc1967919a870765b87db809e6190
Ezra Bathini <ezrabathini@vodafone.com>
2021-03-06 19:28:37 +1300

    clean up

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,15 @@
-import React from 'react'
-import { ThemeProvider } from 'emotion-theming'
-import Header from './Header'
-// import Skills from './Skills'
-// import Dots from './Dots'
-// import Projects from './Projects'
-// import WorkHistory from './WorkHistory'
-import theme from './themes/theme'
+import React from 'react';
+import { ThemeProvider } from 'emotion-theming';
+import Header from './Header';
+
+import theme from './themes/theme';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      {/* <NavBar /> */}
-      {/* <Dots></Dots> */}
       <Header />
-      {/* <Skills />
-      <Projects />
-      <WorkHistory /> */}
     </ThemeProvider>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/Header.tsx
+++ src/Header.tsx
@@ -1,23 +1,18 @@
-import React from 'react'
-import { Flex, Box, Text } from 'rebass'
+import React from 'react';
+import { Flex, Box, Text } from 'rebass';
 
 export default function Header() {
   return (
     <Flex
       sx={{
         backgroundColor: 'primary',
-      }}>
+      }}
+    >
       <Box px={[10, 30, 40]} py={[10, 30, 40]}>
         <Text py={10} variant="custom" color="brightMagenta">
           {'Hi, I am Ezra'}
         </Text>
-        <Text pt={10} variant="custom" color="robinsEgg">
-          {'Front End Developer.'}
-        </Text>
-        <Text py={10} variant="custom" color="robinsEgg">
-          {'Based in Auckland, NZ'}
-        </Text>
       </Box>
     </Flex>
-  )
+  );
 }
